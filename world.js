a7749ce1d782b8ac7099b83426ad1b1c5ac7019d
brandy903 <brandy_anthony@hotmail.com>
2014-11-22 01:12:01 +0000

    third commit

--- world.js
+++ world.js
@@ -13,7 +13,8 @@ window.onload = function(){
 function getidentity(){
     $('lookup').observe('click', function(){ 
 	var term = $("term").getValue(); 
-     new Ajax.Request("world.php", {
+     new Ajax.Request("world.php", 
+     {
                method : 'get',
 			   parameters : {lookup : term},
                onSuccess: function(transport) {
